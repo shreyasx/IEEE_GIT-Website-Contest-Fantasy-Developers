04bb6ae6cc0a1821d12075f535a13519ef1dc59b
Shreyas <shreyasx@protonmail.ch>
2020-07-30 05:56:51 +0530

    overlay update

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    function startOverlay(oid) {
+        $('.overlay-veil').fadeTo("50", '0.8', function () {
+            var element = $('div[data-overlayid="' + oid + '"]');
+            element.fadeIn('50');
+            element.addClass('active');
+        });
+    }
+
+    function closeOverlays(oid) {
+        $('.overlay-component').each(function () {
+            if ($(this).hasClass('active')) {
+                $('.overlay-veil').fadeOut();
+                $(this).fadeOut('50', function () {
+                    $(this).removeClass('active');
+                });
+            }
+        });
+        if (oid !== false) startOverlay(oid);
+    }
+    $('.close').click(function () {
+        closeOverlays(false);
+    });
+    $('.overlay-click').click(function (e) {
+        console.log('clicked');
+        e.preventDefault();
+        var oid = $(this).data('overlay');
+        closeOverlays(oid);
+    });
+});
