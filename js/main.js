95d795aee817da4686404db1c121f9d560929275
Shreyas <shreyasx@protonmail.ch>
2020-08-17 03:30:43 +0530

    sticky

--- js/main.js
+++ js/main.js
@@ -26,3 +26,16 @@ $(document).ready(function () {
         closeOverlays(oid);
     });
 });
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    window.onscroll = function () { myFunction() };
+    var header = document.getElementById("myHeader");
+    var sticky = header.offsetTop;
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+});
